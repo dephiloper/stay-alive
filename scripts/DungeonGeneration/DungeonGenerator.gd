4d3b41097ac6ff368ef425b637bf973315b35da4
Philipp Bönsch <philipp.boensch@softgames.de>
2020-08-08 15:51:39 +0200

    basic skeleton for the dungeon generation fsm

---
+++ scripts/DungeonGeneration/DungeonGenerator.gd
@@ -0,0 +1,26 @@
+class_name DungeonGenerator extends Node2D
+
+onready var _states_map = {
+	'RoomGeneration': $States/RoomGeneration.init(self),
+	'RoomSeparation': $States/RoomSeparation.init(self),
+	'MainRoomPicking': $States/MainRoomPicking.init(self),
+	'Triangulation': $States/Triangulation.init(self),
+	'GraphGeneration': $States/GraphGeneration.init(self),
+	'HallwayRouting': $States/HallwayRouting.init(self),
+	'HallwayGeneration': $States/HallwayGeneration.init(self)
+}
+
+var _state := 'RoomGeneration'
+
+func _ready() -> void:
+	_change_state(_state)
+
+func _physics_process(delta: float) -> void:
+	var state_name = _states_map[_state].process(delta)
+	if state_name != "":
+		_change_state(state_name)
+
+func _change_state(new_state: String) -> void:
+	_states_map[_state].leave()
+	_state = new_state
+	_states_map[_state].enter()
---
+++ scripts/DungeonGeneration/RoomSeparation.gd
@@ -0,0 +1,17 @@
+class_name RoomSeparation extends BaseState
+
+var _generator: DungeonGenerator
+
+func init(root: Node) -> BaseState:
+	_generator = root as DungeonGenerator
+	return .init(root)
+
+func enter() -> void:
+	.enter()
+
+func process(delta: float) -> String:
+	var state := .process(delta)
+	return state
+	
+func leave() -> void:
+	.leave()
